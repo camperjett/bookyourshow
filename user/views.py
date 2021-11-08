583781c3caa7313f1692d8a958c06ef854dd5798
Siddharth Verma <88787415+vsiddharth365@users.noreply.github.com>
2021-11-09 02:08:41 +0530

    Update views.py

--- user/views.py
+++ user/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserChangeForm
 from .forms import UserRegisterForm
 from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
-from django.contrib.auth.decorators import login_required
+from .forms import EditProfileForm
 
-#################### index#######################################
 @login_required
 def index(request):
 	return render(request, 'user/index.html', {'form':form, 'title':'index'})
 
-########### register here #####################################
+
 def register(request):
 	if request.method == 'POST':
 		form = UserRegisterForm(request.POST)
@@ -36,22 +35,37 @@ def register(request):
 			return redirect('login')
 	else:
 		form = UserRegisterForm()
-	return render(request, 'user/register.html', {'form': form, 'title':'reqister here'})
+	return render(request, 'user/register.html', {'form': form, 'title':'register here'})
 
-################ login forms###################################################
+def Logout(request):
+	logout(request)
+	return redirect('login')
+
+
 def Login(request):
 	if request.method == 'POST':
-
-		# AuthenticationForm_can_also_be_used__
-
 		username = request.POST['username']
 		password = request.POST['password']
 		user = authenticate(request, username = username, password = password)
 		if user is not None:
 			form = login(request, user)
-			messages.success(request, f' wecome {username} !!')
+			messages.success(request, f' welcome {username} !!')
 			return redirect('index')
 		else:
-			messages.info(request, f'account done not exit plz sign in')
+			messages.info(request, f'account done not exist plz sign in')
 	form = AuthenticationForm()
 	return render(request, 'user/login.html', {'form':form, 'title':'log in'})
+
+@login_required
+def edit_profile(request):
+	form = EditProfileForm(request.POST or None, instance=request.user)
+	if request.method == 'POST':
+		if form.is_valid():
+			username = request.POST['username']
+			email = request.POST['email']
+			phone_no = request.POST['phone_no']
+			first_name = request.POST['first_name']
+			last_name = request.POST['last_name']
+			form.save()
+			return redirect('index')
+	return render(request, 'user/editprofile.html', {'form':form, 'title':'Edit profile'})
