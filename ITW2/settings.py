97107785b69cc7d160f2adadb207362ad05aa440
Siddharth Verma <88787415+vsiddharth365@users.noreply.github.com>
2021-11-08 17:00:15 +0530

    Update settings.py

--- ITW2/settings.py
+++ ITW2/settings.py
@@ -120,8 +120,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'siddharthverma'
-EMAIL_HOST_PASSWORD = 'vsiddharth365@gmail.com'
+EMAIL_HOST_USER = 'vsiddharth365@gmail.com'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Default primary key field type
