7b604e2c858c357de470d1b38697b312982e933b
Siddharth Verma <88787415+vsiddharth365@users.noreply.github.com>
2021-11-08 17:05:12 +0530

    Update settings.py

--- ITW2/settings.py
+++ ITW2/settings.py
@@ -121,6 +121,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'vsiddharth365@gmail.com'
+EMAIL_HOST_PASSWORD = '*rishabh100'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Default primary key field type
