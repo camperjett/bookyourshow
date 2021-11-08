32237a21cc39fb2af1df748bd14ef3bdb5b21899
Siddharth Verma <88787415+vsiddharth365@users.noreply.github.com>
2021-11-08 16:27:08 +0530

    Update urls.py

--- ITW2/urls.py
+++ ITW2/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pages.views import home_view, about_view, contact_view
-from accounts.views import register_view, login_view, logout_view
+from user import views as user_view
+from django.contrib.auth import views as auth
 urlpatterns = [
     path('', home_view, name='home'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('register/', register_view, name='register'),
+    path('',include('user.urls')),
+    path('login/', user_view.Login, name ='login'),
+    path('logout/', auth.LogoutView.as_view(template_name ='user/index.html'), name ='logout'),
+    path('register/', user_view.register, name ='register'),
     path('about/', about_view, name='about'),
     path('contact/', contact_view, name='contact'),
     path('accounts/', include('allauth.urls')),
