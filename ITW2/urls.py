786c0be041ca4b6c6d3143ee89959564ef1ab898
Siddharth Verma <88787415+vsiddharth365@users.noreply.github.com>
2021-11-08 20:04:16 +0530

    Update urls.py

--- ITW2/urls.py
+++ ITW2/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from pages.views import home_view, about_view, contact_view
+from pages.views import home_view
 from user import views as user_view
 from django.contrib.auth import views as auth
 urlpatterns = [
