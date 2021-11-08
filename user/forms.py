18c0f7898097a880d2515d3418ff023c68ef46b2
Siddharth Verma <88787415+vsiddharth365@users.noreply.github.com>
2021-11-09 02:09:19 +0530

    Update forms.py

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField()
@@ -10,3 +10,18 @@ class UserRegisterForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'phone_no', 'password1', 'password2']
+
+class EditProfileForm(UserChangeForm):
+	email = forms.EmailField()
+	phone_no = forms.CharField(max_length=20, required=False)
+	first_name = forms.CharField(max_length=20, required=False)
+	last_name = forms.CharField(max_length=20, required=False)
+	photo = forms.ImageField(required=False)
+	age = forms.CharField(max_length=5, required=False)
+	gender = forms.CharField(max_length=10, required=False)
+	address = forms.CharField(max_length=30, required=False)
+	class Meta:
+		model = User
+		fields = ['username', 'email','first_name', 'last_name']
+	def get_object(self):
+		return self.request.user
