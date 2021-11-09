95ffb9c97aff6dea30f72b110a78faed71fb72f2
Siddharth Verma <88787415+vsiddharth365@users.noreply.github.com>
2021-11-10 02:22:00 +0530

    Update urls.py

--- movie/urls.py
+++ movie/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from django.conf import settings
-from . import views
+from pages.views import home_view
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('movies', views.landing_page, name='landing_page'),
+    path('movies/', home_view, name='landing_page'),
 ]
