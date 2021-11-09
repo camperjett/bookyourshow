364a64a86fd0840d601ec6175573f61473caacb8
Siddharth Verma <88787415+vsiddharth365@users.noreply.github.com>
2021-11-09 12:13:54 +0530

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('logout/', views.Logout, name ='logout'),
     path('register/', views.register, name ='register'),
     path('editprofile/', views.edit_profile, name='edit_profile'),
+    path('password/', views.change_password.as_view(template_name='user/change_password.html'), name='change_password'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
