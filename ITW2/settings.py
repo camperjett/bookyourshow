95ade1727c0c9be06cee3f7226ae75a0d663aa09
Siddharth Verma <88787415+vsiddharth365@users.noreply.github.com>
2021-11-16 20:59:06 +0530

    Update settings.py

--- ITW2/settings.py
+++ ITW2/settings.py
@@ -44,12 +44,11 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
-    # 'relational_table',
     'user',
     'crispy_forms',
     'movie',
-    # 'show',
-    # 'booking'
+    'chat',
+    'channels'
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -80,7 +79,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ITW2.wsgi.application'
-
+ASGI_APPLICATION = 'ITW2.asgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -89,6 +96,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'NAME': BASE_DIR / 'db_test.sqlite3'
+        },
     }
 }
 
