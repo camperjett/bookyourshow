c74cc9037973a3ac6e44be067d7008ae05befa09
Siddharth Verma <88787415+vsiddharth365@users.noreply.github.com>
2021-11-24 18:07:11 +0530

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from user.models import Account
 
 def index(request):
     return render(request, 'chat/index.html')
 
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name': room_name
+        'room_name': room_name, 'user': request.user
     })
